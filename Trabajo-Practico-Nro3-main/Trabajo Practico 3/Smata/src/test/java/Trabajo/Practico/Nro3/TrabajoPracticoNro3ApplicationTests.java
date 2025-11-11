f62716fbf5fb3d1204d0cfcbd7f499fbafc54585
NicolasAbuin99 <nicolasabuin19@gmail.com>
2025-11-11 13:20:39 -0300

    Add files via upload

---
+++ Trabajo-Practico-Nro3-main/Trabajo Practico 3/Smata/src/test/java/Trabajo/Practico/Nro3/TrabajoPracticoNro3ApplicationTests.java
@@ -0,0 +1,13 @@
+package Trabajo.Practico.Nro3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoPracticoNro3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
