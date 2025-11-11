6c045a75976e33bc9502543cd9a4d74a137c5bf3
NicolasAbuin99 <nicolasabuin19@gmail.com>
2025-11-11 11:22:04 -0300

    Add files via upload

---
+++ Trabajo Practico 3/Smata/src/main/java/Trabajo/Practico/Nro3/TrabajoPracticoNro3Application.java
@@ -0,0 +1,13 @@
+package Trabajo.Practico.Nro3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoPracticoNro3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoPracticoNro3Application.class, args);
+	}
+
+}
